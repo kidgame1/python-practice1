fa44cbe8df3a40d517f532e410cb4f5c1d5cf7ac
kidgame1 <kidgame1@gmail.com>
2022-02-19 09:58:19 +0800

    Update student.py

--- student.py
+++ student.py
@@ -1,14 +1,12 @@
-class Student:
+from person import Person
+
+
+from person import Person
+class Student(Person):
     def __init__(self,name,age,school):
         self.name = name
         self.age = age
         self.school = school
-
-    def print_name(self):
-        print(self.name)
-
-    def print_age(self):
-        print(self.age)
 
     def print_school(self):
         print(self.school)
